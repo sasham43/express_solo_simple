32ba67457a678ec419a2721d07203897daef9473
sasham43 <sashasemail@gmail.com>
2016-04-05 11:24:30 -0500

    Serve simple html with jquery and css

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var path = require('path');
+
 var app = express();
 var port = 3000;
+
+app.use(express.static('public'));
 
 app.listen(port,function(){
   console.log('Server listening on port ' + port + '...');
@@ -8,7 +12,8 @@ app.listen(port,function(){
 
 app.get('/', function(req, res){
   console.log('Received GET request');
-  res.send(' You made a GET request!');
+  //res.send('You made a GET request!');
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.post('/', function(req, res){
