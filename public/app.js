81cdee5313c9ddd87d512f63bbb64c7e77c89088
sasham43 <sashasemail@gmail.com>
2016-04-05 14:02:46 -0500

    Wall of gifs

--- public/app.js
+++ public/app.js
@@ -1,3 +1,27 @@
 $(function(){
-  console.log('jquery loaded');
+  // console.log('jquery loaded');
+
+  $("button").on('click', function(){
+    var gifTitle = $("#gif").val();
+
+    var gifBaseURL = 'http://api.giphy.com/v1/gifs/search?q=';
+    var apiKey = '&api_key=dc6zaTOxFJmzC';
+    var gifURL = gifBaseURL + gifTitle + apiKey;
+
+    $.ajax({
+      url: gifURL,
+      type: 'GET'
+    }).done(function(response){
+      // get array of responses
+      var gifArray = response.data;
+      console.log(gifArray);
+
+      // loop through array and append images to div
+      for(var it=0; it < gifArray.length; it++){
+          var imageURL = gifArray[it].images.fixed_height.url;
+          var imageElement = '<img class=\"img-fluid\" src=\"' + imageURL + '\"/>'
+          $(".gifContainer").append(imageElement);
+         }
+    });
+  });
 })
